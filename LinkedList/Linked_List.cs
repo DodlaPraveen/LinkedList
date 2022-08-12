e5fdb3591d8aac3984d1dafd808d347ea833727f
praveen dodla <dodlapraveen09@gmail.com>
2022-08-12 18:05:04 +0530

    Remove First Element in LinkedList

--- LinkedList/Linked_List.cs
+++ LinkedList/Linked_List.cs
@@ -71,5 +71,14 @@ namespace LinkedList
                 temp = temp.next;
             }
         }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
     }
 }
