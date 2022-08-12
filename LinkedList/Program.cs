4fee9d96d97c068d3721d43823c56a3c6d8a6e02
praveen dodla <dodlapraveen09@gmail.com>
2022-08-12 18:02:06 +0530

    Insert 30 between 56 and 70

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -9,8 +9,10 @@ namespace LinkedList
             Console.WriteLine("***Welcome_To_LinkedList***");
             Linked_List list = new Linked_List();
             list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Display();
+            Console.WriteLine("\nAfter Inserting Element...");
+            list.InserAtParticularPosition(2, 30);
             list.Display();
             Console.ReadLine();
         }
