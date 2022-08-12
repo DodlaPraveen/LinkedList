2a3cef376f8b97483d06797b94d94433ed09c373
praveen dodla <dodlapraveen09@gmail.com>
2022-08-12 17:49:48 +0530

    Appending elements to the LinkedList

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -8,10 +8,9 @@ namespace LinkedList
         {
             Console.WriteLine("***Welcome_To_LinkedList***");
             Linked_List list = new Linked_List();
-            Console.WriteLine("Add_To_Begining");
-            list.Add(70);
-            list.Add(30);
             list.Add(56);
+            list.Add(30);
+            list.Add(70);
             list.Display();
             Console.ReadLine();
         }
