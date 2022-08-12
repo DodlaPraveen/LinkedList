10ce41f972f704ac3534f55b6bb356900b527c03
praveen dodla <dodlapraveen09@gmail.com>
2022-08-12 17:36:30 +0530

    Creating simple LinkedList

---
+++ LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Welcome_To_LinkedList***");
+            Linked_List list = new Linked_List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
